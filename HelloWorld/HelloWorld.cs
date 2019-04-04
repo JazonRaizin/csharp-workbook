a3328d3cbde30ee56af91d9160f4d1563f44707a
Jazon Garza <Jazon.Garza@gmail.com>
2019-04-04 18:28:32 -0500

    Week1-1 turning in

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -7,6 +7,14 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Jazon");
+            Console.WriteLine("What is your name?");
+            yourname = Console.ReadLine("Hello " + yourname);
+            if {(yourname = "Bob")
+            Console.WriteLine("Really your name is bob")
+            }else{
+            Console.WriteLine("Hello " + yourname ", it is nice to meet you")
+            }
         }
     }
 }
